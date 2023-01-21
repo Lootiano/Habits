8362dd17689975f2a9ba992e4e829717c24ed1f6
Lootiano <lucianograbin12@gmail.com>
2023-01-21 20:43:47 -0300

    correcting typing error

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const form = document.querySelector('#form-habits');
-const nlwSetup = new NLWSetup(form);
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
 
-const button = document.querySelector('header button');
+const button = document.querySelector("header button")
 
-button.addEventListener('click', add);
-form.addEventListener('change', save);
+button.addEventListener("click", add)
+form.addEventListener("change", save)
 
 function add() {
-  const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
-  const dayExists = nlwSetup.dayExists(today);
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
 
   if (dayExists) {
-    alert('Dia ja incluso');
-    return;
+    alert("Dia já incluso")
+    return
   }
-  alert('Adicionado com sucesso');
-  nlwSetup.addDay(today);
+  alert("Adicionado com sucesso")
+  nlwSetup.addDay(today)
 }
 
 function save() {
-  localStorage.setItem('NLWSetup@Habits', JSON.stringify(nlwSetup.data));
+  localStorage.setItem("NLWSetup@Habits", JSON.stringify(nlwSetup.data))
 }
 
-const data = JSON.parse(localStorage.getItem('NLWSetup@Habits')) || {};
-nlwSetup.setData(data);
-nlwSetup.load();
+const data = JSON.parse(localStorage.getItem("NLWSetup@Habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
